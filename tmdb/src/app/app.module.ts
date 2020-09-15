870d641bd6486143bac61a8016ccd0da4611493c
Set Israel Ortiz Monasterio <pixrael777@gmail.com>
2020-09-14 21:57:49 -0300

    added movie results component

--- tmdb/src/app/app.module.ts
+++ tmdb/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+
+import { MovieResultsComponent } from './movie-results/movie-results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieResultsComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ tmdb/src/app/movie-results/movie-results.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MovieResultsComponent } from './movie-results.component';
+
+describe('MovieResultsComponent', () => {
+  let component: MovieResultsComponent;
+  let fixture: ComponentFixture<MovieResultsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MovieResultsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MovieResultsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
