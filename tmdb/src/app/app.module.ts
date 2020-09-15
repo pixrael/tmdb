a838e6d4464ff0fa53878b34b598287080c4eb45
Set Israel Ortiz Monasterio <pixrael777@gmail.com>
2020-09-14 22:27:01 -0300

    created components with placeholders

--- tmdb/src/app/app.module.ts
+++ tmdb/src/app/app.module.ts
@@ -1,21 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+
 
 import { MovieResultsComponent } from './movie-results/movie-results.component';
+import { MovieSearchComponent } from './movie-search/movie-search.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MovieResultsComponent
+    MovieResultsComponent,
+    MovieSearchComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatTableModule
+    FormsModule,
+    MatButtonModule,
+    MatTableModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
