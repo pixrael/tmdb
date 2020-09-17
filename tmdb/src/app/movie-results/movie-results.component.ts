2deff3366003220d65a55631044ef835668b6bfd
Set Israel Ortiz Monasterio <pixrael777@gmail.com>
2020-09-16 21:43:49 -0300

    formatted data to display in table

--- tmdb/src/app/movie-results/movie-results.component.ts
+++ tmdb/src/app/movie-results/movie-results.component.ts
@@ -9,12 +9,15 @@ export interface ResultElement {
   title: string;
   releaseDate: string;
   poster: string;
-  genre: string;
-  position: number;
+  genreId: number;
+  genreIds: number[];
 }
 
 const ELEMENT_DATA: ResultElement[] = [
-  { releaseDate: '1988-07-21', title: 'Bat*21', poster: '/yIeFT2RqJdXMRIhGLs05xtzBkt9.jpg', genre: '28', position: 1 },
+  {
+    releaseDate: '1988-07-21',
+    title: 'Bat*21', poster: '/yIeFT2RqJdXMRIhGLs05xtzBkt9.jpg', genreId: 8, genreIds: [1, 2]
+  },
 
 ];
 
@@ -24,7 +27,7 @@ const ELEMENT_DATA: ResultElement[] = [
   styleUrls: ['./movie-results.component.scss']
 })
 export class MovieResultsComponent implements OnInit {
-  displayedColumns: string[] = ['genre', 'title', 'releaseDate', 'poster'];
+  displayedColumns: string[] = ['genreId', 'title', 'releaseDate', 'poster'];
   dataSource = ELEMENT_DATA;
 
   genresIds = [28, 12, 16, 35, 80, 99, 18, 10751, 14, 36, 27]; // this should como its own request
@@ -34,26 +37,19 @@ export class MovieResultsComponent implements OnInit {
   ngOnInit(): void {
     this.movieDataService.getMovies$().pipe(filter(movies => !!movies)).subscribe(movies => {
 
-      console.log('results ', movies);
-
-
-      const groupedMovies = _.groupBy(movies.results, (result) => {
-
-        let resultGenreId = 0;
-
-        _.forEach(this.genresIds, genreId => {
-          if (result.genre_ids.some((gId) => gId === genreId)) {
-            console.log('return ', genreId);
-            resultGenreId = genreId;
-          }
-        });
-
-        return resultGenreId;
-
+      const movs = movies.results.map(movie => {
+        return {
+          releaseDate: movie.release_date,
+          title: movie.title,
+          poster: movie.poster_path,
+          genreIds: movie.genre_ids,
+        };
       });
 
-      console.log('groupedMovies ', groupedMovies);
-      this.mapGroupedMoviesToTable(groupedMovies);
+
+
+      this.dataSource = this.groupByGenreIds(this.genresIds, movs);
+
     });
 
   }
@@ -70,4 +66,36 @@ export class MovieResultsComponent implements OnInit {
     this.dataSource = mappedMovies;
   }
 
+  private groupByGenreIds(genres, entries): any {
+
+    const result = {};
+    entries.forEach(entry => {
+
+      genres.forEach(genre => {
+
+        if (entry.genreIds.some(genreId => genreId === genre)) {
+          if (!result[genre]) {
+            result[genre] = [];
+          }
+
+          result[genre].push({ genreId: genre, ...entry });
+        }
+      });
+
+    });
+
+
+    const dataSource = [];
+
+    genres.forEach(genre => {
+
+      if (result[genre]) {
+        dataSource.push(...result[genre]);
+      }
+    });
+
+
+    return dataSource;
+
+  }
 }
