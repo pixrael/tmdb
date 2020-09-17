a283a02085bad040c50a7a8eaa746d1bf65a860c
Set Israel Ortiz Monasterio <pixrael777@gmail.com>
2020-09-17 20:16:38 -0300

    added placeholder group header

--- tmdb/src/app/movie-results/movie-results.component.ts
+++ tmdb/src/app/movie-results/movie-results.component.ts
@@ -14,17 +14,25 @@ export interface PeriodicElement {
   symbol: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
+export interface GroupBy {
+  group: string;
+  isGroupBy: boolean;
+}
+
+const ELEMENT_DATA: (PeriodicElement | GroupBy)[] = [
+  { isGroupBy: true, group: 'Group 1' },
   { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
   { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
   { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
   { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
   { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { isGroupBy: true, group: 'Group 2' },
   { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
   { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
   { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
   { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
   { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  { isGroupBy: true, group: 'Group 3' },
   { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
   { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
   { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
@@ -45,7 +53,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class MovieResultsComponent implements OnInit, AfterViewInit {
 
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  dataSource = new MatTableDataSource<PeriodicElement | GroupBy>(ELEMENT_DATA);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
@@ -74,6 +82,10 @@ export class MovieResultsComponent implements OnInit, AfterViewInit {
 
     }); */
 
+  }
+
+  isGroup(index, item): boolean {
+    return item.isGroupBy;
   }
 
   ngAfterViewInit(): void {
